8c4e4933096d41b953364f6666f6ac4ec91f6b1d
juanjorgegarcia <juan.j.garcia007@gmail.com>
2020-09-01 17:06:57 -0300

    Raised errors got a better description

--- parser.py
+++ parser.py
@@ -20,7 +20,7 @@ class Parser:
 
             if Parser.tokens.actual._type == 'INT':
                 raise NameError(
-                    'INVALID GRAMMAR: Two Numbers separated only by spaces')
+                    'INVALID GRAMMAR: Two Numbers separated only by spaces in position: ({Parser.tokens.position})')
             while Parser.tokens.actual._type == 'PLUS' or Parser.tokens.actual._type == 'MINUS':
                 if Parser.tokens.actual._type == 'PLUS':
                     Parser.tokens.selectNext()
@@ -28,15 +28,18 @@ class Parser:
                     if Parser.tokens.actual._type == 'INT':
                         res += int(Parser.tokens.actual.value)
                     else:
-                        raise NameError('TOKEN INVALIDO')
+                        raise NameError(
+                            f"INVALID TOKEN: token type espected {'INT'}, instead got {Parser.tokens.actual._type} in position: ({Parser.tokens.position}) ")
                 if Parser.tokens.actual._type == 'MINUS':
                     Parser.tokens.selectNext()
 
                     if Parser.tokens.actual._type == 'INT':
                         res -= int(Parser.tokens.actual.value)
                     else:
-                        raise NameError('TOKEN INVALIDO')
+                        raise NameError(
+                            f"INVALID TOKEN: token type espected {'INT'}, instead got {Parser.tokens.actual._type} in position: ({Parser.tokens.position})")
                 Parser.tokens.selectNext()
             return res
         else:
-            raise NameError('TOKEN INVALIDO')
+            raise NameError(
+                f'INVALID TOKEN: ({Parser.tokens.actual.value}) in position: ({Parser.tokens.position})')
